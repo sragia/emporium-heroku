fa0a82f2d3a6f2e7c46347b5b92cb639871fea78
Aigars Bedeicis <aigars.bedeicis@edu.rtu.lv>
2017-03-17 15:13:57 +0200

    migration

--- db/migrate/20170215193528_create_books_and_authors_books.rb
+++ db/migrate/20170215193528_create_books_and_authors_books.rb
@@ -15,23 +15,6 @@ create_table :authors_books, :id => false do |table|
 table.column :author_id, :integer, :null => false
 table.column :book_id, :integer, :null => false
 end
-say_with_time 'Adding foreign keys' do
-execute 'CREATE TRIGGER fk_bk_authors BEFORE DELETE ON authors ' +
-'FOR EACH ROW ' +
-'BEGIN ' +
-'DELETE FROM authors_books WHERE author_id = OLD.id; ' +
-'END'
-execute 'CREATE TRIGGER fk_bk_books BEFORE DELETE ON books ' +
-'FOR EACH ROW ' +
-'BEGIN ' +
-'DELETE FROM authors_books WHERE book_id = OLD.id; ' +
-'END'
-execute 'CREATE TRIGGER fk_books_publishers BEFORE DELETE ON publishers ' +
-'FOR EACH ROW ' +
-'BEGIN ' +
-'DELETE FROM books WHERE publisher_id = OLD.id; ' +
-'END'
-end
 end
 def self.down
 drop_table :authors_books
